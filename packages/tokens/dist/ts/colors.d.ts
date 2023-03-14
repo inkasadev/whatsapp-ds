22360f38cd6104a081364d0f1cc825c25ea40d4b
Phillipe Martins <hi@inkasa.dev>
2023-03-14 02:28:30 -0300

    fix: gitignore file

---
+++ packages/tokens/dist/ts/colors.d.ts
@@ -0,0 +1,33 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 14 Mar 2023 03:55:26 GMT
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value: any;
+  name?: string;
+  comment?: string;
+  themeable?: boolean;
+  attributes?: {
+    category?: string;
+    type?: string;
+    item?: string;
+    subitem?: string;
+    state?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+declare const tokens: {
+  "color": {
+    "green-ultradark": DesignToken,
+    "green-darkgreen": DesignToken,
+    "green-normal": DesignToken,
+    "green-light": DesignToken,
+    "blue-normal": DesignToken,
+    "white-normal": DesignToken
+  }
+}
